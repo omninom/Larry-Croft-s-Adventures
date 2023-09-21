e548b5d4138c320a27d0d284b6be6eb894d3d749
Neeraj Patel <114114241+neerajpatel1234@users.noreply.github.com>
2023-09-21 16:38:53 +1200

    Neeraj: - Added skeleton code for tests on Recorder classes. - Fixed some minor indentation issues. - Fixed some line lengths to >100 chars. - Added some javaDoc comments.

--- src/main/java/nz/ac/wgtn/swen225/lc/recorder/Recorder.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/recorder/Recorder.java
@@ -5,8 +5,7 @@ import java.util.Map;
  * Recorder.java
  *
  * <p>This class is a class for the Recorder program.
- * It will crete a new instance of the Recorder at the start of the program.
- *
+ * It will create a new instance of the Recorder at the start of the program.
  */
 public class Recorder {
     // ----------------------------------- VARIABLES ----------------------------------- //
@@ -14,7 +13,6 @@ public class Recorder {
     static boolean isRecording = false;
     Map<Integer, RecordItem> currentRecording;
     Map<Integer, Map<Integer, RecordItem>> allRecordings;
-
 
     // ----------------------------------- CONSTRUCTOR ----------------------------------- //
 
@@ -29,7 +27,7 @@ public class Recorder {
 
     /**
      * This method will set up the recorder.
-     * It will set the replay speed to 0 (manual replay) wait for the user to start recording.
+     * It will set the replay speed to 0 (manual replay) and wait for the user to start recording.
      */
     private void setUpRecorder() {
         // ---- set up the recorder ---- //
@@ -61,12 +59,13 @@ public class Recorder {
      */
     public void startRecording() {
         System.out.println("Recorder: Recording has started. \n");
-        currentRecording = new HashMap<Integer, RecordItem>();
+        currentRecording = new HashMap<>();
         int currentSequenceNumber = 0;
 
         // ---- Record the initial game ---- //
         if (isRecording) {
-            RecordItem initialGame = new RecordItem(currentSequenceNumber, "0,0", "0,0", "INITIAL_GAME", "NONE", "NONE");
+            RecordItem initialGame = new RecordItem(currentSequenceNumber, "0,0",
+                    "0,0", "INITIAL_GAME", "NONE", "NONE");
             currentRecording.put(currentSequenceNumber, initialGame);
 
             System.out.println("  Recorder: Initial game has been added:  [ " + initialGame + " ] \n");
@@ -76,18 +75,18 @@ public class Recorder {
 
         while (isRecording) {
             // ---- Some move made ---- //
-            RecordItem currentChange = new RecordItem(currentSequenceNumber, "0,0", "0,0", "MOVE_MADE", "NONE", "NONE");
+            RecordItem currentChange = new RecordItem(currentSequenceNumber, "0,0",
+                    "0,0", "MOVE_MADE", "NONE", "NONE");
             currentRecording.put(currentSequenceNumber, currentChange);
 
             System.out.println("  Recorder: Move has been added: [ " + currentChange + " ] \n");
-
-
         }
+
         System.out.println("Recorder: Recording has stopped. \n");
     }
 
     /**
-     * When called this will replay the recording in step when the user presses a button.
+     * When called this will replay the recording in steps when the user presses a button.
      */
     public void stepByStepReplay() {
 
@@ -96,22 +95,19 @@ public class Recorder {
     /**
      * When called will replay the recording at the speed set by the user.
      * If the user has not set a speed, it will default to the slowest speed.
-     *
      */
     public void autoReplay() {
 
-
     }
 
-
     // ----------------------------------- GETTERS ----------------------------------- //
+
     public boolean getIsRecording() {
         return isRecording;
     }
 
-
-
     // ----------------------------------- SETTERS ----------------------------------- //
+
     /**
      * This method will set the isRecording variable.
      *
@@ -129,5 +125,4 @@ public class Recorder {
     public void setReplaySpeed(int speed) {
         this.replaySpeed = speed;
     }
-
 }
