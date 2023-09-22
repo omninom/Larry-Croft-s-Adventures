7951e17b26f184f26c548efe0e769bb6dd2bd27d
JebDudfield <110765820+JebDudfield@users.noreply.github.com>
2023-09-22 12:41:12 +1200

    Reformatted.

--- src/test/java/test/nz/ac/wgtn/swen225/lc/fuzz/MockEnvoy.java
+++ src/test/java/test/nz/ac/wgtn/swen225/lc/fuzz/MockEnvoy.java
@@ -4,7 +4,7 @@ package java.test.nz.ac.wgtn.swen225.lc.fuzz;
  * Mock envoy used before App is developed enough for a true envoy.
  */
 public class MockEnvoy implements AppEnvoy {
-  public void move(){
+  public void move() {
 
   }
 }
