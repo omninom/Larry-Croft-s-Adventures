5a51472b8790dfdc2800391c4d0ae01a938c0d47
JebDudfield <110765820+JebDudfield@users.noreply.github.com>
2023-09-22 12:41:12 +1200

    Reformatted.

--- src/test/java/test/nz/ac/wgtn/swen225/lc/fuzz/AppEnvoy.java
+++ src/test/java/test/nz/ac/wgtn/swen225/lc/fuzz/AppEnvoy.java
@@ -1,8 +1,8 @@
 package java.test.nz.ac.wgtn.swen225.lc.fuzz;
 
 /**
- * Defines ways in which the FuzzTest class can interact with the game.
- * Introduced to assist with refactoring and allow for development of the FuzzTest module before the App module is finished.
+ * Defines ways in which the FuzzTest class can interact with the game. Introduced to assist with
+ * refactoring and allow for development of the FuzzTest module before the App module is finished.
  */
 public interface AppEnvoy {
 
--- src/test/java/test/nz/ac/wgtn/swen225/lc/fuzz/FuzzTest.java
+++ src/test/java/test/nz/ac/wgtn/swen225/lc/fuzz/FuzzTest.java
@@ -4,7 +4,8 @@ import java.awt.event.KeyEvent;
 import org.junit.jupiter.api.Test;
 
 /**
- * Provides fuzz testing by providing semi-random inputs to the App module in order to provoke errors.
+ * Provides fuzz testing by providing semi-random inputs to the App module in order to provoke
+ * errors.
  */
 public class FuzzTest {
 
@@ -12,7 +13,7 @@ public class FuzzTest {
    * Tests on level 1.
    */
   @Test
-  public void test1(){
+  public void test1() {
     // TODO random movement testing
     // TODO weighted movement testing
     // TODO pause/resume testing (in quick succession, make inputs while paused)
@@ -20,9 +21,9 @@ public class FuzzTest {
   }
 
   /**
-   * Tests on level 2.
-   * Level 2 has much added complexity due to unknown bonus actors.
+   * Tests on level 2. Level 2 has much added complexity due to unknown bonus actors.
    */
   @Test
-  public void test2(){}
+  public void test2() {
+  }
 }
