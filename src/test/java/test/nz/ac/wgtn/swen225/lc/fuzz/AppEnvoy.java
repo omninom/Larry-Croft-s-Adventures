155d46b9b60c08346b380d52e700539a6050bec1
JebDudfield <110765820+JebDudfield@users.noreply.github.com>
2023-09-24 22:10:44 +1300

    Groundwork and standards compliance.

--- src/test/java/test/nz/ac/wgtn/swen225/lc/fuzz/AppEnvoy.java
+++ src/test/java/test/nz/ac/wgtn/swen225/lc/fuzz/AppEnvoy.java
@@ -3,6 +3,7 @@ package test.nz.ac.wgtn.swen225.lc.fuzz;
 /**
  * Defines ways in which the FuzzTest class can interact with the game. Introduced to assist with
  * refactoring and allow for development of the FuzzTest module before the App module is finished.
+ * TODO Introduce new methods to retrieve information for smarter testing.
  */
 public interface AppEnvoy {
 
