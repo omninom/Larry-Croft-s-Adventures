d220ee010df3a519a9c4d6e24cf4627497998d90
Jack Gallagher <gallagjack1@ecs.vuw.ac.nz>
2023-09-24 22:21:06 +1300

    Reformatted App code

--- src/main/java/nz/ac/wgtn/swen225/lc/app/AppInput.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/AppInput.java
@@ -4,11 +4,11 @@ package nz.ac.wgtn.swen225.lc.app;
  * Represents an input the app can receive.
  */
 public enum AppInput {
-    MOVE_UP,
-    MOVE_DOWN,
-    MOVE_LEFT,
-    MOVE_RIGHT,
+  MOVE_UP,
+  MOVE_DOWN,
+  MOVE_LEFT,
+  MOVE_RIGHT,
 
-    PAUSE,
-    UNPAUSE,
+  PAUSE,
+  UNPAUSE,
 }
