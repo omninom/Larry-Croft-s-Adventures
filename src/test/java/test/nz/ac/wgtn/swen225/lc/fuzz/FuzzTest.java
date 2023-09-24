e46b33e60b83c8d043f0fa4a1fcf3aaefb12bba5
JebDudfield <110765820+JebDudfield@users.noreply.github.com>
2023-09-24 22:01:20 +1300

    Exported level testing to shared method.

--- src/test/java/test/nz/ac/wgtn/swen225/lc/fuzz/FuzzTest.java
+++ src/test/java/test/nz/ac/wgtn/swen225/lc/fuzz/FuzzTest.java
@@ -13,20 +13,36 @@ public class FuzzTest {
   //Number of random steps to take on each level.
   private final int steps = 100;
   //Number of pause/unpause cycles to execute in one wave.
-  private final int pauses = 20;
+  private final int pauses = 30;
 
   /**
    * Tests on level 1.
    */
   @Test
   public void test1() {
+    testLevel(1);
+  }
+
+  /**
+   * Tests on level 2. Level 2 has much added complexity due to unknown bonus actors.
+   */
+  @Test
+  public void test2() {
+    testLevel(2);
+  }
+
+  /**
+   * Tests an arbitrary level, if it exists, otherwise throws an error.
+   * @param level the index of the level we wish to test.
+   */
+  public void testLevel(int level){
     // random movement testing
     // TODO weighted movement testing
     // pause/resume testing (in quick succession, make inputs while paused)
     // TODO setup/teardown, level loading and detection of success.
     for (int i = 0; i < iterations; i++) {
       //TODO Setup
-      AppEnvoy envoy = getNewEnvoy();
+      AppEnvoy envoy = getNewEnvoy(level);
       for (int step = 0; step < steps; step++) {
         //5% chance of pausespam test.
         if(Math.random() < 0.5){
@@ -51,19 +67,21 @@ public class FuzzTest {
           default:
             throw new IllegalArgumentException();
         }
+        if(envoy.isStopped()){
+          envoy.printSuccessMessage();
+          return;
+        }
       }
     }
   }
 
   /**
-   * Tests on level 2. Level 2 has much added complexity due to unknown bonus actors.
+   * Generates an AppEnvoy for the tests to use to interact with the App.
+   * @param level the level to load. An error should be thrown if this is an invalid level.
+   * @return An AppEnvoy.
    */
-  @Test
-  public void test2() {
-    test1();
-  }
-
-  private AppEnvoy getNewEnvoy() {
+  private AppEnvoy getNewEnvoy(int level) {
+    // TODO check level is real.
     return new MockEnvoy();
   }
 }
