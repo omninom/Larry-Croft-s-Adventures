45c0b8ba451251910a6af1b069b0c6f523afb1d6
Jack Gallagher <gallagjack1@ecs.vuw.ac.nz>
2023-09-24 21:54:44 +1300

    Window and input handling stub

---
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/App.java
@@ -0,0 +1,72 @@
+package nz.ac.wgtn.swen225.lc.app;
+
+import java.io.File;
+
+/**
+ * The App module.
+ */
+public class App {
+    public App() {
+
+    }
+
+    /**
+     * Handles an input.
+     * @param inputType the input to handle.
+     * @return whether the input was valid.
+     */
+    public boolean handleInput(AppInput inputType) {
+        System.out.println("[APP DEBUG] Recieved input '" + inputType + "'");
+
+        // TODO: Translate / Validate input with Domain
+        // TODO: Record input
+
+        return true;
+    }
+
+    /**
+     * Starts a new game.
+     * @param level - the level to start at.
+     */
+    public void newGame(int level) {
+        System.out.println("[APP DEBUG] New game: Level " + level);
+    }
+
+    /**
+     * Saves the game.
+     * @param file the file to save to.
+     * @return whether saving was successful.
+     */
+    public boolean saveGame(File file) {
+        System.out.println("[APP DEBUG] Saving game to " + file.getAbsolutePath());
+
+        // TODO: Call persistency module
+        return true;
+    }
+
+    /**
+     * Loads the game.
+     * @param file the file to load.
+     * @return whether loading was successful.
+     */
+    public boolean loadGame(File file) {
+        System.out.println("[APP DEBUG] Loading game from " + file.getAbsolutePath());
+
+        // TODO: Call persistency module
+        return true;
+    }
+
+    /**
+     * Ends the game.
+     */
+    public void endGame() {
+        System.out.println("[APP DEBUG] Ending game");
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        AppWindow window = new AppWindow(app);
+
+        window.setVisible(true);
+    }
+}
