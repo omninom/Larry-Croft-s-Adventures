b9517d7d17c727fef994cb9a1caa38970c02f891
omninom <40103480+omninom@users.noreply.github.com>
2023-09-25 20:10:40 +1300

    Added tile type colour mapping
    -need someway of storing the current "board" so I can render it.
    - Added hypothetical updateRenderer() method to see how it would look like working with a "Board" object
    - NOTE: Simple tile colours will not exist in the final implementation, this is purely for development.

--- src/main/java/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/renderer/Renderer.java
@@ -1,5 +1,8 @@
 package nz.ac.wgtn.swen225.lc.renderer;
 
+import nz.ac.wgtn.swen225.lc.domain.*;
+
+import java.util.HashMap;
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,8 +12,24 @@ public class Renderer extends JPanel {
     private static final Color PLAYER_COLOR = Color.RED;
     private int playerX; // Column position of the player
     private int playerY; // Row position of the player
+    private static final HashMap<TileType, Color> TILE_COLORS = new HashMap<>();    // Map of tile types to colors
 
-    public Renderer() {
+    static {
+        TILE_COLORS.put(TileType.WALL, Color.BLACK);
+        TILE_COLORS.put(TileType.FREE, Color.WHITE);
+        TILE_COLORS.put(TileType.KEY, Color.YELLOW);
+        TILE_COLORS.put(TileType.LOCKED_DOOR, Color.GRAY);
+        TILE_COLORS.put(TileType.INFO, Color.BLUE);
+        TILE_COLORS.put(TileType.TREASURE, Color.ORANGE);
+        TILE_COLORS.put(TileType.EXIT_LOCK, Color.PINK);
+        TILE_COLORS.put(TileType.EXIT, Color.GREEN);
+
+    }
+
+    private Board maze; // We need something like this to get the level to render
+
+    public Renderer(Board maze) {       // Constructor would need to take in a level to render
+        this.maze = maze;
         setLayout(new GridLayout(GRID_SIZE, GRID_SIZE));
         initializeGrid();
         playerX = GRID_SIZE / 2; // Put player in center for now
@@ -27,6 +46,30 @@ public class Renderer extends JPanel {
                 add(cell);
             }
         }
+    }
+
+    private Color getTileColor(TileType type) {
+        return TILE_COLORS.getOrDefault(type, Color.WHITE); // Default to white for unknown tile types
+    }
+
+    // Update the renderer when the game state changes, e.g., after player moves
+    public void updateRenderer(Board updatedGameBoard) {
+        this.maze = updatedGameBoard;
+        // Repaint the grid with updated tile types/colors
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                JPanel cell = (JPanel) getComponentAtPosition(i, j);
+                //cell.setBackground( somehow get the tile type from the level );
+            }
+        }
+        // Remove the player from the old position
+        JPanel oldCell = (JPanel) getComponentAtPosition(playerY, playerX);
+        assert oldCell != null;
+        oldCell.removeAll();
+        // Draw the player at the new position
+        drawPlayer();
+
+        repaint();
     }
 
     // Method to draw the player
@@ -59,7 +102,7 @@ public class Renderer extends JPanel {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Renderer");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().add(new Renderer());
+            //frame.getContentPane().add(new Renderer());
             frame.setSize(500, 500);
             frame.setResizable(false);
             frame.setVisible(true);
