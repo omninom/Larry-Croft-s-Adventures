bcd828bf20a2e276d871ad77d89887c1881ba427
Neeraj Patel <114114241+neerajpatel1234@users.noreply.github.com>
2023-09-25 14:39:25 +1300

    - Fixed indentation issues to 2 spaces. - Cleared all checkstyle issues.

--- src/test/java/test/nz/ac/wgtn/swen225/lc/recorder/RecorderTests.java
+++ src/test/java/test/nz/ac/wgtn/swen225/lc/recorder/RecorderTests.java
@@ -7,73 +7,73 @@ import org.junit.jupiter.api.Test;
  * This is a collection of tests for the Recorder class.
  * It will test the methods in the Recorder class to ensure that they are working as expected.
  * It will test the following methods:
- *   - setUpRecorder()
- *   - startRecording()
- *   - stepByStepReplay()
- *   - autoReplay()
- *   - setIsRecording()
- *   - getIsRecording()
+ * - setUpRecorder()
+ * - startRecording()
+ * - stepByStepReplay()
+ * - autoReplay()
+ * - setIsRecording()
+ * - getIsRecording()
  */
 public class RecorderTests {
-    /**
-     * Test that the JUnit tests are working.
-     */
-    @Test
-    void test_Junit_Working() {
-        Assertions.assertTrue(true);
-    }
+  /**
+   * Test that the JUnit tests are working.
+   */
+  @Test
+  void test_Junit_Working() {
+    Assertions.assertTrue(true);
+  }
 
-    /**
-     * Test that the setUpRecorder() method is working as expected.
-     * This will test that the replaySpeed is set to 0 at the start.
-     */
-    @Test
-    void test_setUpRecorder() {
+  /**
+   * Test that the setUpRecorder() method is working as expected.
+   * This will test that the replaySpeed is set to 0 at the start.
+   */
+  @Test
+  void test_setUpRecorder() {
 
-    }
+  }
 
-    /**
-     * Test that the startRecording() method is working as expected.
-     * This will test that the startRecording() method is working as expected.
-     */
-    @Test
-    void test_startRecording() {
+  /**
+   * Test that the startRecording() method is working as expected.
+   * This will test that the startRecording() method is working as expected.
+   */
+  @Test
+  void test_startRecording() {
 
-    }
+  }
 
-    /**
-     * Test that the stepByStepReplay() method is working as expected.
-     * This will test that the stepByStepReplay() method is working as expected.
-     */
-    @Test
-    void test_stepByStepReplay() {
+  /**
+   * Test that the stepByStepReplay() method is working as expected.
+   * This will test that the stepByStepReplay() method is working as expected.
+   */
+  @Test
+  void test_stepByStepReplay() {
 
-    }
+  }
 
-    /**
-     * Test that the autoReplay() method is working as expected.
-     * This will test that the autoReplay() method is working as expected.
-     */
-    @Test
-    void test_autoReplay() {
+  /**
+   * Test that the autoReplay() method is working as expected.
+   * This will test that the autoReplay() method is working as expected.
+   */
+  @Test
+  void test_autoReplay() {
 
-    }
+  }
 
-    /**
-     * Test that the setIsRecording() method is working as expected.
-     * This will test that the setIsRecording() method is working as expected.
-     */
-    @Test
-    void test_setIsRecording() {
-    }
+  /**
+   * Test that the setIsRecording() method is working as expected.
+   * This will test that the setIsRecording() method is working as expected.
+   */
+  @Test
+  void test_setIsRecording() {
+  }
 
-    /**
-     * Test that the getIsRecording() method is working as expected.
-     * This will test that the getIsRecording() method is working as expected.
-     */
-    @Test
-    void test_getIsRecording() {
+  /**
+   * Test that the getIsRecording() method is working as expected.
+   * This will test that the getIsRecording() method is working as expected.
+   */
+  @Test
+  void test_getIsRecording() {
 
-    }
+  }
 }
 
