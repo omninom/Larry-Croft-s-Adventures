a533e6f6960eba382a84e5f0e072eb0249cb43b0
Benjamin Park <parkbenj@ecs.vuw.ac.nz>
2023-10-08 19:50:45 +1300

    Update Documentation for FileHandler and fix javaDoc Checkstyle issues.

--- src/main/java/nz/ac/wgtn/swen225/lc/persistency/FileHandler.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/persistency/FileHandler.java
@@ -1,14 +1,24 @@
 package nz.ac.wgtn.swen225.lc.persistency;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import nz.ac.wgtn.swen225.lc.domain.*;
 
+/**
+ * Handles file operations for saving and loading levels.
+ *
+ * @author Benjamin Park
+ */
 public class FileHandler {
+  /**
+   * Saves the supplied maze.
+   *
+   * @param maze The maze object to be saved.
+   * @param file The file object to save maze to.
+   * @throws IOException
+   */
   public void save(Maze maze, File file) throws IOException {
     ObjectMapper mapper = new ObjectMapper();
     String json = mapper.writeValueAsString(maze);
@@ -21,6 +31,12 @@ public class FileHandler {
     System.out.println("Save successful");
   }
 
+  /**
+   * Loads a maze Level.
+   *
+   * @param fileName the name of the JSON level file.
+   * @return Maze level
+   */
   public Maze loadMaze(String fileName) {
     File file = new File(fileName);
     Maze maze;
