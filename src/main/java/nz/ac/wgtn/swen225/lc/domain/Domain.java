b5cbebf18a7b0bc584f21a17e11970d92be01eb6
Riley <riley@westsnz.com>
2023-10-11 13:53:45 +1300

    Adding JUnit tests for Domain.
    Small Changes in domain class to add methods for unit tests

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Domain.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Domain.java
@@ -186,12 +186,21 @@ public class Domain {
     return failed;
   }
 
+  //FUNCTIONS FOR JUNIT TESTS
   public void setFailed(boolean set){
     this.failed = set;
   }
 
   public void setWon(boolean set){
     this.won = set;
+  }
+
+  public Maze getMaze(){
+    return this.maze;
+  }
+
+  public void setMaze(Maze m){
+    this.maze = m;
   }
 
 }
