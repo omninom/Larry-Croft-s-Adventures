95d327d360af27284dc5deea0fc6376c0c8359d7
Jack Gallagher <gallagjack1@ecs.vuw.ac.nz>
2023-10-13 20:42:21 +1300

    AppInput javadoc

--- src/main/java/nz/ac/wgtn/swen225/lc/app/AppInput.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/AppInput.java
@@ -6,11 +6,33 @@ package nz.ac.wgtn.swen225.lc.app;
  * @author Jack Gallagher (300615528)
  */
 public enum AppInput {
+  /**
+   * Move the player up.
+   */
   MOVE_UP,
+
+  /**
+   * Move the player down.
+   */
   MOVE_DOWN,
+
+  /**
+   * Move the player left.
+   */
   MOVE_LEFT,
+
+  /**
+   * Move the player right.
+   */
   MOVE_RIGHT,
 
+  /**
+   * Pause the game.
+   */
   PAUSE,
+
+  /**
+   * Unpause the game.
+   */
   UNPAUSE,
 }
